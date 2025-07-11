642ab0c69ef1b5446db14eb47cb3e5222208a58f
Mike Rudy <rudymiked@gmail.com>
2025-07-11 14:50:18 -0700

    updating banners

--- rudy-software-consulting/src/components/Banner.tsx
+++ rudy-software-consulting/src/components/Banner.tsx
@@ -5,62 +5,111 @@ const Banner: React.FC = () => {
         <Box sx={{ py: 6, position: 'relative' }}>
             {/* Background image */}
             <Box sx={{ width: '100%' }}>
-                <img 
-                    src="src/assets/tech.jpg" 
-                    alt="Banner Image" 
-                    style={{ width: '100%', height: 'auto', borderRadius: 8 }} 
+                <img
+                    src="src/assets/tech.jpg"
+                    alt="Banner Image"
+                    style={{ width: '100%', height: 'auto', borderRadius: 8 }}
                 />
             </Box>
-
-            {/* Overlay box - positioned on top right of the image */}
-            <Box
-                sx={ (theme) =>({
-                    position: 'absolute',
-                    top: 100,
-                    right: 20,
-                    backgroundColor: 'rgba(255, 255, 255, 0.9)',
-                    padding: 3,
-                    borderRadius: 2,
-                    maxWidth: 300,
-                    boxShadow: 3,
-                    [theme.breakpoints.down('sm')]: {
-                    padding: 2,
-                    height: '40%',
-                    justifyContent: 'flex-start',
-                    },
-                })}
-            >
-                <Typography 
-                    variant="h4"
-                    component="h1"
-                    gutterBottom   
+            <Grid container spacing={2}>
+                {/* Overlay box - positioned on top right of the image */}
+                <Box
                     sx={(theme) => ({
-                    [theme.breakpoints.down('sm')]: {
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    fontSize: '1.2rem', // Optional: scale down font size
-                    },
-                })}>
-                    Software Solutions and Technology Consulting
-                </Typography>
-                <Typography 
-                    variant="body1" 
-                    color="text.secondary"   
+                        position: 'absolute',
+                        top: 100,
+                        left: 20,
+                        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+                        padding: 3,
+                        borderRadius: 2,
+                        maxWidth: 300,
+                        boxShadow: 3,
+                        [theme.breakpoints.down('sm')]: {
+                            padding: 2,
+                            height: '40%',
+                            justifyContent: 'flex-start',
+                        },
+                    })}
+                >
+                    <Box>
+                        <Typography
+                            variant="h4"
+                            component="h1"
+                            gutterBottom
+                            sx={(theme) => ({
+                                [theme.breakpoints.down('sm')]: {
+                                    whiteSpace: 'nowrap',
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                    fontSize: '1.2rem', // Optional: scale down font size
+                                },
+                            })}>
+                            Software Solutions and Technology Consulting
+                        </Typography>
+                        <Typography
+                            variant="body1"
+                            color="text.secondary"
+                            sx={(theme) => ({
+                                [theme.breakpoints.down('sm')]: {
+                                    whiteSpace: 'nowrap',
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis',
+                                    fontSize: '1.2rem', // Optional: scale down font size
+                                },
+                            })}>
+                            Transform your ideas into reality with expert custom tailored software. Our services include custom software development for web, desktop and mobile, SaaS, API integrations, and enterprise consulting.
+                        </Typography>
+                        <Button variant="contained" color="primary" href="#contact" sx={{ mt: 1 }}>
+                            Get Started
+                        </Button>
+                    </Box>
+                </Box>
+                {/* <Box
                     sx={(theme) => ({
-                    [theme.breakpoints.down('sm')]: {
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    fontSize: '1.2rem', // Optional: scale down font size
-                    },
-                })}>
-                    Transform your ideas into reality with expert custom tailored software. Our services include custom software development for web, desktop, and mobile, SaaS, API integrations, and enterprise consulting.
-                </Typography>
-                <Button variant="contained" color="primary" href="#contact" sx={{ mt: 1 }}>
-                    Get Started
-                </Button>
-            </Box>
+                        position: 'absolute',
+                        top: 130,
+                        right: 20,
+                        boxShadow: 0,
+                        [theme.breakpoints.down('sm')]: {
+                            padding: 2,
+                            height: '40%',
+                            justifyContent: 'flex-start',
+                        },
+                    })}
+                >
+                    <img src="/src/assets/dash.png" style={{ width: 450, height: 'auto' }} />
+                </Box>
+                */}
+                <Box
+                    sx={(theme) => ({
+                        position: 'absolute',
+                        top: 120,
+                        right: 75,
+                        boxShadow: 0,
+                        [theme.breakpoints.down('sm')]: {
+                            padding: 2,
+                            height: '40%',
+                            justifyContent: 'flex-start',
+                        },
+                    })}
+                >
+                    <img src="/src/assets/marathon.png" style={{ width: 450, height: 'auto' }} />
+                </Box>
+                <Box
+                    sx={(theme) => ({
+                        position: 'absolute',
+                        top: 70,
+                        right: 20,
+                        boxShadow: 0,
+                        [theme.breakpoints.down('sm')]: {
+                            padding: 2,
+                            height: '40%',
+                            justifyContent: 'flex-start',
+                        },
+                    })}
+                >
+                    <img src="/src/assets/realestatedash.png" style={{ width: 450, height: 'auto' }} />
+                </Box>
+            </Grid>
         </Box>
     );
 }
