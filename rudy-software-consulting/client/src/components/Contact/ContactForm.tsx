c2aff71e14385adab8462c7d3c5950ac32524ac7
Mike Rudy <rudymiked@gmail.com>
2025-07-17 19:55:12 -0700

    rename VITE_API_URL

--- rudy-software-consulting/client/src/components/Contact/ContactForm.tsx
+++ rudy-software-consulting/client/src/components/Contact/ContactForm.tsx
@@ -46,7 +46,7 @@ const ContactForm: React.FC = () => {
   const handleSubmit = (e: any) => {
     e.preventDefault();
     
-    fetch(`http://${import.meta.env.API_URL}/contact`, {
+    fetch(`http://${import.meta.env.VITE_API_URL}/contact`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
