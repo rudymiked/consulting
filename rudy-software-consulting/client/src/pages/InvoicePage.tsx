af28dc216828ba6c0a395042309f71a35230e188
Mike Rudy <rudymiked@gmail.com>
2025-12-05 11:25:11 -0800

    invoice page view

--- rudy-software-consulting/client/src/pages/InvoicePage.tsx
+++ rudy-software-consulting/client/src/pages/InvoicePage.tsx
@@ -2,9 +2,19 @@ import { Box, Container } from "@mui/material";
 import Invoice from "../components/Invoice/Invoice";
 import { useParams } from "react-router-dom";
 import React from "react";
+import { useAppInsights } from "../services/Telemtry/AppInsightsProvider";
 
 const InvoicePage: React.FC = () => {
     const { invoiceId: invoiceId } = useParams<{ invoiceId: string }>();
+    const appInsights = useAppInsights();
+
+      React.useEffect(() => {
+        appInsights.trackEvent({ name: 'Invoice_Visit_' + invoiceId }, {
+          timestamp: new Date().toISOString(),
+          userAgent: navigator.userAgent,
+        });
+        appInsights.trackPageView({ name: 'Invoice', uri: window.location.pathname });
+      }, [appInsights]);
 
     return (
         <Box sx={{ pt: 9, pb: 7, backgroundColor: '#f0f4f8' }}>
